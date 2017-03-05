b82fd7d726eb3ad922b05d47f0b4d52820f17abb
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-05 18:45:32 +1100

    hooked up redux thunk

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+
+
+export function signinUser({ email, password }) {
+  return function(dispatch) {
+    // Submit email/password to the server
+
+    // If request is good we
+    // - update state to indicate user is authenticated
+    // - save the JWT token
+    // - redirect to the route '/feature'
+
+
+    // If request is bad we
+    // - show an error to the user
+  }
+}
