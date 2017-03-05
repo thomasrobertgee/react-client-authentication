3931811f8caccc55dbc3e77e3d6b08238498eac2
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-06 10:43:27 +1100

    added signout ability

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { browserHistory } from 'react-router'
 import {
   AUTH_USER,
-  AUTH_ERROR
+  AUTH_ERROR,
+  UNAUTH_USER
  } from './types'
 
 const ROOT_URL = 'http://localhost:3090'
@@ -32,5 +33,13 @@ export function authError(error) {
   return {
     type: AUTH_ERROR,
     payload: error
+  }
+}
+
+export function signoutUser() {
+  localStorage.removeItem('token')
+  
+  return {
+    type: UNAUTH_USER
   }
 }
