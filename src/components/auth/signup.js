923a390e9252373590978d431c6d492b2b99ec0e
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-06 11:16:37 +1100

    client side password confirm validation with error display

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,11 +3,61 @@ import { reduxForm } from 'redux-form'
 import * as actions from '../../actions'
 
 class Signup extends Component {
+  handleFormSubmit({ email, password }) {
+    // Need to do something to log user in
+    this.props.signupUser({ email, password })
+  }
+
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong>Oops!</strong> {this.props.errorMessage}
+        </div>
+      )
+    }
+  }
+
   render() {
+    const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props
     return (
-      <div>Sign Up</div>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input {...email} className="form-control" />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input {...password} type="password" className="form-control" />
+          {password.touched && password.error && <div className="error">{password.error}</div>}
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Confirm Password:</label>
+          <input {...passwordConfirm} type="password" className="form-control" />
+        </fieldset>
+        {this.renderAlert()}
+        <button action="submit" className="btn btn-primary">Sign Up</button>
+      </form>
     )
   }
 }
 
-export default Signup
+function validate(formProps) {
+  const errors = {}
+
+  if (formProps.password != formProps.passwordConfirm) {
+    errors.password = 'Passwords don`t match'
+  }
+
+  return errors
+}
+
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error}
+}
+
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate // same as validate: validate
+}, mapStateToProps, actions)(Signup)
